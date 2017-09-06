06d63a2fd16237cf53845e053968d2ec07b8742d
Claire Follett <chlaw101@gmail.com>
2017-09-06 16:12:15 -0700

    home page almost done

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 // load the things we need
+var path = require('path');
 var express = require('express');
 var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+//getting the image files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // use res.render to load up an ejs view file
+app.get('/', function(req, res) {
+	res.render('pages/product');
+});
+
+app.listen(8083);
+console.log('8083 is the magic port');
